18e5258c9111f894bb6e5f5cc11f08327fc7e17e
Maxim Talanov <mail@maxtalanov.ru>
2022-07-31 17:59:20 +0300

    Настройка проекта

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="App__o">
+            <h1>Привет реакт</h1>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
