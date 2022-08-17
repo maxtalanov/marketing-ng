2016a75ee2373e341b399161d51436ed550d6525
Maxim Talanov <mail@maxtalanov.ru>
2022-08-17 23:24:44 +0300

    Создать компонент Typography

---
+++ src/components/ui/Typography/Typography.tsx
@@ -0,0 +1,21 @@
+import "./Typography.scss"
+import {FC} from "react";
+
+const TYPOGRAPHY_CLASS: string = 'typography';
+
+interface TypographyProps {
+    tag?: string;
+    children: any;
+}
+
+const Typography:FC<TypographyProps> = ({tag, children}) => {
+    switch (tag) {
+        case 'h1': return (<h1>{children}</h1>);
+        case 'h2': return (<h2>{children}</h2>);
+        case 'h3': return (<h3>{children}</h3>);
+
+        default: return (<h1>{children}</h1>);
+    }
+}
+
+export default Typography;
