948e428fe4eba647d71467e208b70ef38855f508
Maxim Talanov <mail@maxtalanov.ru>
2022-08-17 22:19:38 +0300

    Добавить стили для изображения и правого контейнера

--- src/containers/HappyClients/HappyClients.tsx
+++ src/containers/HappyClients/HappyClients.tsx
@@ -8,23 +8,21 @@ const HAPPYCLIENTS_CLASS: string = 'happy-clients';
 const HappyClients: FC = () => {
     return (
         <section className={`${HAPPYCLIENTS_CLASS}`} style={{display: "flex", justifyContent: "center", alignItems: "center", padding: "10%"}}>
-            {/*<div className={`${HAPPYCLIENTS_CLASS}__container`}>*/}
-            {/*    <h3 className={`${HAPPYCLIENTS_CLASS}__title`}>Happy Clients</h3>*/}
-            {/*    <p className={`${HAPPYCLIENTS_CLASS}__subTitle`}>*/}
-            {/*        The Marketing Accountability Standards Board (MASB) endorses the definitions, purposes, and*/}
-            {/*        constructs of classes*/}
-            {/*    </p>*/}
-            {/*</div>*/}
-            {/*<div className={`${HAPPYCLIENTS_CLASS}__container`}>*/}
-            {/*    <div className={`${HAPPYCLIENTS_CLASS}__left`}></div>*/}
-            {/*    <div className={`${HAPPYCLIENTS_CLASS}__right`}>*/}
-            {/*        <span className={`${HAPPYCLIENTS_CLASS}__figure-semicircle`}></span>*/}
-            {/*        <span className={`${HAPPYCLIENTS_CLASS}__figure-square`}></span>*/}
-            {/*        <img className={`${HAPPYCLIENTS_CLASS}__img`} src={photo} alt="Изображение человека"/>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-
-            <One />
+            <div className={`${HAPPYCLIENTS_CLASS}__container`}>
+                <h3 className={`${HAPPYCLIENTS_CLASS}__title`}>Happy Clients</h3>
+                <p className={`${HAPPYCLIENTS_CLASS}__subTitle`}>
+                    The Marketing Accountability Standards Board (MASB) endorses the definitions, purposes, and
+                    constructs of classes
+                </p>
+            </div>
+            <div className={`${HAPPYCLIENTS_CLASS}__container`}>
+                <div className={`${HAPPYCLIENTS_CLASS}__left`}></div>
+                <div className={`${HAPPYCLIENTS_CLASS}__right`}>
+                    <span className={`${HAPPYCLIENTS_CLASS}__figure-semicircle`}></span>
+                    <span className={`${HAPPYCLIENTS_CLASS}__figure-square`}></span>
+                    <img className={`${HAPPYCLIENTS_CLASS}__img`} src={photo} alt="Изображение человека"/>
+                </div>
+            </div>
         </section>
     )
 }
