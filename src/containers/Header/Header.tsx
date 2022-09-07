5554135a40050d2ecc7ffa1301351d38d0658b4d
Maxim Talanov <mail@maxtalanov.ru>
2022-09-08 02:26:00 +0300

    Авто форматирование кода

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,20 +1,21 @@
-import {FC} from "react";
+import { FC } from "react";
 
 import "./Header.scss";
 import Logotype from "../../components/ui/Logotype/Logotype";
 import Navbar from "../../components/ui/Navbar/Navbar";
 import Clients from "../../components/Clients/Clients";
-import {companyLogos} from "../../utils/constans";
+import { companyLogos } from "../../utils/constans";
 const HEADER_CLASS: string = 'header';
 
-const Header:FC = () => {
-  return(
-      <header className={`${HEADER_CLASS}`}>
-          <Logotype />
-          <Navbar />
+const Header: FC = () => {
+  return (
+    <header className={`${HEADER_CLASS}`}>
 
-          <Clients arrIMG={companyLogos} />
-      </header>
+      <Logotype />
+      <Navbar />
+
+      <Clients arrIMG={companyLogos} />
+    </header>
   )
 }
 
