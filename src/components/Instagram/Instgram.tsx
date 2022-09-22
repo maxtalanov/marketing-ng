aecd99644a43ca1450f4fb1eafc3d4da4dc05b6a
Maxim Talanov <mail@maxtalanov.ru>
2022-09-22 16:45:17 +0300

    feat: модификаия компонента instagram

---
+++ src/components/Instagram/Instgram.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import Emoji from "./card/Emoji/Emoji";
+import Follower from "./card/Follower/Follower";
+import MediaType from "./card/MediaType/Media-Type";
+import Profaile from "./card/Profile/Profile";
+import "./Instagram.scss";
+const INSTAGRAM_CLASS: string = 'instagram';
+
+const Instagram:FC = () => {
+
+    return (
+        <div className={`${INSTAGRAM_CLASS}`}>
+        
+            <div className={`${INSTAGRAM_CLASS}__container ${INSTAGRAM_CLASS}__container_type_profile`}>
+                <Profaile />
+            </div>
+
+            <div className={`${INSTAGRAM_CLASS}__container ${INSTAGRAM_CLASS}__container_type_media`}>  
+                <MediaType />
+            </div>
+
+            <div className={`${INSTAGRAM_CLASS}__container ${INSTAGRAM_CLASS}__container_type_emoji`}>
+                <Emoji />
+            </div>
+
+            <div className={`${INSTAGRAM_CLASS}__container ${INSTAGRAM_CLASS}__container_type_follower`}>
+                <Follower />
+            </div>
+
+            <div className={`${INSTAGRAM_CLASS}__container ${INSTAGRAM_CLASS}__container_type_new-follower`}>
+                
+            </div>
+            
+            
+            {/* <MediaType /> */}
+            
+        </div>
+    )
+}
+
+export default Instagram;
