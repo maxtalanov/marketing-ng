c2a76556a9f366051198a03a8c912155f8012863
Maxim Talanov <mail@maxtalanov.ru>
2022-09-26 03:17:33 +0300

    ref: Изменить класс стилей на корректный

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -5,7 +5,7 @@ import Footer from '../Footer/Footer';
 function App() {
 
     return (
-        <div className="App">
+        <div className="ng-marceting">
           <Header />
           <Footer />
         </div>
