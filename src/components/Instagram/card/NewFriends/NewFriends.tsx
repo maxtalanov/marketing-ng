386bdafb97ef1dfa33d5c5359f3bcce45cce1201
Maxim Talanov <mail@maxtalanov.ru>
2022-10-05 03:44:05 +0300

    feat: Добавить нужные импорты и верстку

--- src/components/Instagram/card/NewFriends/NewFriends.tsx
+++ src/components/Instagram/card/NewFriends/NewFriends.tsx
@@ -1,12 +1,19 @@
 import { FC } from "react";
-import './NewFriends';
+import './NewFriends.scss';
+import InstagramHeader from "../../Instagram-Header/Instaram-Header";
 
 const newFRENDS_CLASS: String = 'new-frends';
 
 const NewFriends: FC = () => {
     return (
-        <div>
-
+        <div className={`${newFRENDS_CLASS}`}>
+            <InstagramHeader type={'newFrends'} title={'Follower Growth'}/>
+            <h3 className={`${newFRENDS_CLASS}__title`}>
+                16.2k{'\u00A0'}
+                <span className={`${newFRENDS_CLASS}__span`}>
+                    New Followers
+                </span>
+            </h3>
         </div>
     )
 }
