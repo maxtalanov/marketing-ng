107a0461abf92e8c5ffccdd5357435e6f7b3857e
Maxim Talanov <mail@maxtalanov.ru>
2022-10-18 10:29:58 +0300

    refactor(navbar): Дать возможность генерировать разметку
    из входящего потока данных

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -5,11 +5,59 @@ import Select from "../Select/Select";
 
 const NAVBAR_CLASS: string = 'navbar';
 
-interface NavbarProps {
-    theme?: string,
+const DATA_NAVBAR_moc: NavbarOption[] = [
+    {
+        path: '/solution',
+        namePath: 'Solution',
+        option: true,
+        options: [
+            {
+                path: '/tab_1',
+                namePath: 'Tab 1',
+                option: false,
+            },
+            {
+                path: '/tab_2',
+                namePath: 'Tab 2',
+                option: false,
+            },
+            {
+                path: '/tab_3',
+                namePath: 'Tab 3',
+                option: false,
+            },
+        ],
+    },
+    {
+        path: '/plans',
+        namePath: 'Plans',
+        option: false,
+    },
+    {
+        path: '/resource',
+        namePath: 'Resource',
+        option: false,
+    },
+    {
+        path: '/blog',
+        namePath: 'Blog',
+        option: false,
+    },
+];
+
+export interface NavbarOption {
+    path: string,
+    namePath: string,
+    option: boolean
+    options?: NavbarOption[];
 }
 
-const Navbar: FC<NavbarProps> = ({ theme = 'grey' }) => {
+interface NavbarProps {
+    theme?: string,
+    data?: NavbarOption[],
+}
+
+const Navbar: FC<NavbarProps> = ({ theme = 'grey', data = DATA_NAVBAR_moc, }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const onClickOptions = () => {
@@ -19,27 +67,26 @@ const Navbar: FC<NavbarProps> = ({ theme = 'grey' }) => {
     return (
         <nav className={`${NAVBAR_CLASS}`}>
             <ul className={`${NAVBAR_CLASS}__list`}>
-                <li className={`${NAVBAR_CLASS}__el`}>
-                    <Select type="white" onClick={onClickOptions} isOpen={isOpen} title={'Solution'} />
-                </li>
-                <li className={`${NAVBAR_CLASS}__el`}>
-                    <a
-                        href="#"
-                        className={`${NAVBAR_CLASS}__link ${NAVBAR_CLASS}__link_text_theme_${theme}`}
-                    >Plans</a>
-                </li>
-                <li className={`${NAVBAR_CLASS}__el`}>
-                    <a
-                        href="#"
-                        className={`${NAVBAR_CLASS}__link ${NAVBAR_CLASS}__link_text_theme_${theme}`}
-                    >Resource</a>
-                </li>
-                <li className={`${NAVBAR_CLASS}__el`}>
-                    <a
-                        href="#"
-                        className={`${NAVBAR_CLASS}__link ${NAVBAR_CLASS}__link_text_theme_${theme}`}
-                    >Blog</a>
-                </li>
+                {
+                    data.map((data, index) => {
+                        return (
+                            <li key={index} className={`${NAVBAR_CLASS}__el`}>
+                                {
+                                    data.option
+                                        ? <Select
+                                            type="white"
+                                            onClick={onClickOptions}
+                                            isOpen={isOpen}
+                                            title={data.namePath}
+                                            options={data.options} />
+                                        : <a
+                                            href={data.path}
+                                            className={`${NAVBAR_CLASS}__link ${NAVBAR_CLASS}__link_text_theme_${theme}`}
+                                        >{data.namePath}</a>
+                                }
+                            </li>
+                        )
+                    })}
             </ul>
         </nav>
     )
